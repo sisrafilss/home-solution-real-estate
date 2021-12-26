1c3e20d997705a445a4397428068bcd748c24cb9
Israfil Hossen <sisrafilss@gmail.com>
2021-12-26 18:51:37 +0600

    .env file added

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA6obQAFnfcTVEkDX5o6pjS9UDff5XGeAU",
-  authDomain: "home-solution-ih.firebaseapp.com",
-  projectId: "home-solution-ih",
-  storageBucket: "home-solution-ih.appspot.com",
-  messagingSenderId: "941774801121",
-  appId: "1:941774801121:web:8fbd095cbd57843c680fa0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 export default firebaseConfig;
